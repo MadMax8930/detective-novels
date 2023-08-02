2440a1b88c4e2637d13b44a447db2e5a4710b9f3
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-08-02 15:43:21 +0200

    api routes x4

---
+++ pages/api/novels/[novelId].ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import serverAuth from '@/lib/serverAuth'
+import prismadb from '@/lib/prismadb'
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+   if (req.method !== 'GET') { res.status(405).end() }
+
+   try {
+     await serverAuth(req, res);
+
+     const { novelId } = req.query
+     if(typeof novelId !== 'string') { throw new Error('Invalid ID') }
+     if(!novelId) { throw new Error('Invalid ID') }
+
+     const novel = await prismadb.novel.findUnique({ where: { id: novelId } })
+     if(!novel) { throw new Error('Invalid ID') }
+
+     return res.status(200).json(novel);
+   } catch (error) {
+     console.log(error)
+     res.status(400).end();
+   }
+}
---
+++ pages/api/novels/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import serverAuth from '@/lib/serverAuth'
+import prismadb from '@/lib/prismadb'
+
+export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+   if (req.method !== 'GET') { res.status(405).end() }
+
+   try {
+     await serverAuth(req, res);
+
+     const novels = await prismadb.novel.findMany()
+     return res.status(200).json(novels);
+   } catch (error) {
+     console.log(error)
+     res.status(400).end();
+   }
+}
