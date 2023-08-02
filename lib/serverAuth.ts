b58663fff796ab3824e78909639e1a19d4851f2c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-08-02 15:38:27 +0200

    middleware auth

---
+++ lib/serverAuth.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import prismadb from '@/lib/prismadb'
+
+// Authenticate routes
+const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
+   const session = await getServerSession(req, res, authOptions)
+   if (!session?.user?.email) { throw new Error('User not authenticated') }
+
+   const currentUser = await prismadb.user.findUnique({ where: { email: session.user.email } })
+   if (!currentUser) { throw new Error('User not found') }
+
+   return { currentUser };
+}
+
+export default serverAuth;
