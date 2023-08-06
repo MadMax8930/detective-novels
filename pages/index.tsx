e4513a69a3394cf23b69f17b37c13fec0d570b15
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-08-06 13:57:27 +0200

    novel preview page + update orm + footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,22 @@
 import { Navbar, Footer } from '@/components'
-import NovelList from '@/components/NovelList'
 import useNovelList from '@/hooks/useNovelList'
-// import InfoModal from '@/components/InfoModal'
-// import useInfoModal from '@/hooks/useInfoModal'
+import useInfoModal from '@/hooks/useInfoModal'
+import NovelList from '@/components/NovelList'
+import InfoModal from '@/components/InfoModal'
 
 export default function Home() {
    const { data: novels = [] } = useNovelList();
-   // const { isOpen, closeModal } = useInfoModal();
+   const { isOpen, closeModal } = useInfoModal();
 
    return (
-      <div className='h-[100vh] flex items-center justify-center bg-white-main'>
+      <div className='flex items-center justify-center bg-white-main'>
          <div className='container h-full mx-auto xl:px-30 max-w-6xl'>
-            {/* <InfoModal visible={isOpen} onClose={closeModal} /> */}
+            <InfoModal visible={isOpen} onClose={closeModal} />
             <Navbar />
-            <div className='h-[100px]'></div>
+            <div className='h-[150px]'></div>
             <NovelList header="Novels Trending Now" data={novels} />
-            <Footer />
+            <div className='h-[40px]'></div>
+            <Footer bgLight={true} />
          </div>
       </div>
    )
---
+++ pages/read/[novelId].tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import useNovel from '@/hooks/useNovel'
+import { BsArrowLeftSquareFill, BsPersonSquare } from 'react-icons/bs'
+import { Footer } from '@/components'
+
+const Read = () => {
+   const router = useRouter();
+   const { novelId } = router.query;
+   const { data } = useNovel(novelId as string);
+
+   return (
+      <div className="w-screen bg-[#121212]">
+         {/* Top Navigation */}
+         <div className="fixed w-full px-24 z-10 flex flex-col justify-between gap-8 bg-black bg-opacity-70">
+            <nav className="flex flex-row justify-between">
+               <div className="flex flex-row justify-center px-12 py-5 gap-2 cursor-pointer" onClick={() => router.push('/')}>
+                  <BsArrowLeftSquareFill size={28} onClick={() => router.push('/')} className="text-white cursor-pointer" />
+                  <span className="text-white text-xl font-semibold">Novel Preview:{" "}</span>          
+               </div>
+               <div className="flex px-7 py-5 gap-2 cursor-pointer" onClick={() => router.push('/auth')}>
+                  <span className="text-white text-xl font-semibold">Read the rest</span>
+                  <BsPersonSquare size={28} className="text-white" />
+               </div>
+            </nav>
+         </div>
+         {/* Preview */}
+         <div className="pt-24 px-36 text-justify">
+            <p className="text-green-400 text-1xl md:text-3xl font-bold">{data?.title}</p><br /> 
+            <div className="text-white text-base font-serif">
+               {data?.preview}
+            </div>  
+         </div>
+         {/* Footer */}
+         <div className='flex items-center justify-center'>
+            <div className='container h-full mx-auto xl:px-30 max-w-6xl pt-6'>
+               <Footer bgLight={false} />
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default Read
