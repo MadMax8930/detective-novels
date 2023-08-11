b21aa00cad3edbb087cfc2dfa77e744b204213d0
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-08-12 00:45:01 +0200

    admin auth

--- lib/adminAuth.ts
+++ lib/adminAuth.ts
@@ -6,31 +6,27 @@ type AdminJwtPayload = {
    adminId: string;
 };
 
+// Admin middleware
 const adminAuth = (handler: NextApiHandler) => async (req: NextApiRequest, res: NextApiResponse) => {
-
-   console.log("adminAuth middleware triggered");
+   console.log("adminAuth middleware triggered on adminForm Submit");
    
    try {
-      // Extract the token from the Authorization header
-      const token = req.headers.authorization?.split(' ')[1];
-      if (!token) { return res.status(401).json({ error: 'Unauthorized access' }) }
+      const cookieToken = req.cookies['next-auth.admin-token'];
+      console.log("cookieToken from Authorization Header:", cookieToken); 
+      if (!cookieToken) { return res.status(401).json({ error: 'Unauthorized access. No Token' }) }
 
       // Verify the token and extract the payload
-      const decodedToken = jwt.verify(token, `${process.env.ADMIN_JWT_SECRET}`) as AdminJwtPayload;
+      const decodedToken = jwt.verify(cookieToken, `${process.env.ADMIN_JWT_SECRET}`) as AdminJwtPayload;
       console.log("Decoded Token:", decodedToken); 
 
-      // Fetch the admin from the database using the decoded token's adminId
-      const admin = await prismadb.admin.findUnique({
-         where: { id: String(decodedToken.adminId) }
-      });
+      const admin = await prismadb.admin.findUnique({ where: { id: String(decodedToken.adminId) } });
+      if (!admin) { return res.status(401).json({ error: 'Unauthorized access. No adminId' }) }
 
-      if (!admin) { return res.status(401).json({ error: 'Unauthorized access' }) }
-
-      // User is authorized to proceed to the actual API handler
-      return handler(req, res);
+      // User authorized to proceed to the actual API handler
+      handler(req, res);
    } catch (error) {
       console.error(error);
-      return res.status(500).json({ error: 'A server error has occurred' });
+      return res.status(500).json({ error: 'adminAuth server error has occurred' });
    }
 };
 
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { GetServerSideProps } from 'next';
+import { getSession } from 'next-auth/react'
+import { Carousel, AdminForm } from '@/components'
+import useNovelList from '@/hooks/useNovelList'
+import Cookies from 'js-cookie'
 
-const admin = () => {
-  return (
-    <div>admin</div>
-  )
+interface AdminProps {
+   customToken: string;
 }
 
-export default admin
+export const Admin: React.FC<AdminProps> = ({ customToken }) => {
+   const { data: novels = [] } = useNovelList();
+   const [token, setToken] = useState(customToken);
+
+   useEffect(() => {
+      const retrievedToken = Cookies.get('next-auth.admin.token');
+      console.log('Fetched Token:', retrievedToken);
+      if (retrievedToken) { setToken(retrievedToken); }
+    }, []);
+
+   return (
+      <div className="w-screen min-h-full bg-white-main">
+         <div className="flex flex-col gap-4">
+            <Carousel novels={novels} />
+            <AdminForm token={token} />
+         </div>
+      </div>
+   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+   const session = await getSession(context);
+
+      if (!session?.user?.email) {
+         return {
+            redirect: {
+               destination: '/',
+               permanent: false,
+            },
+         };
+      }
+      
+   const user: any = fetch('/api/current').then(res => res.json())
+
+      if (!user?.adminId) {
+         return {
+            redirect: {
+               destination: '/profile',
+               permanent: false,
+            },
+         };
+      }
+
+   const customToken = context.req.cookies['next-auth.admin-token'] || '';
+
+   return {
+      props: {
+         customToken,
+      },
+   };
+};
+
+export default Admin
+
+
