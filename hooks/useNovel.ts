f3ee9f545f942c0e86a6111a315f74a3ef3dc2e0
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-08-13 05:56:32 +0200

    hook mutate update

--- hooks/useNovel.ts
+++ hooks/useNovel.ts
@@ -2,13 +2,13 @@ import useSWR from 'swr'
 import fetcher from '@/lib/fetcher'
 
 const useNovel = (id?: string) => {
-   const { data, error, isLoading } = useSWR(id ? `/api/novels/${id}` : null, fetcher, {
+   const { data, error, isLoading, mutate } = useSWR(id ? `/api/novels/${id}` : null, fetcher, {
       revalidateIfStale: false,
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
    });
 
-   return { data, error, isLoading };
+   return { data, error, isLoading, mutate };
 }
 
 export default useNovel;
--- hooks/useNovelList.ts
+++ hooks/useNovelList.ts
@@ -2,13 +2,13 @@ import useSWR from 'swr'
 import fetcher from '@/lib/fetcher'
 
 const useNovelList = () => {
-   const { data, error, isLoading } = useSWR('/api/novels', fetcher, {
+   const { data, error, isLoading, mutate } = useSWR('/api/novels', fetcher, {
       revalidateIfStale: false,
       revalidateOnFocus: false,
       revalidateOnReconnect: false,
    });
 
-   return { data, error, isLoading };
+   return { data, error, isLoading, mutate };
 };
 
 export default useNovelList;
