a265f9e596e12673c11681afea4dbfe44e9fcb5d
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-11-27 23:14:03 +0100

    testing

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -5,17 +5,20 @@ import prismadb from '@/lib/prismadb'
 
 // User middleware: Handles authentication and retrieves current user data
 const serverAuth = async (req: NextApiRequest, res: NextApiResponse) => {
-   const session = await getServerSession(req, res, authOptions);
-   console.log('ServerAuth Session:', session);
-   if (!session?.user?.email) { throw new Error('User not authenticated.') }
+   try {
+      const session = await getServerSession(req, res, authOptions);
+      if (!session?.user?.email) { throw new Error('User not authenticated.') }
 
-   const currentUser = await prismadb.user.findUnique({ 
-      where: { email: session.user.email },
-      select: { id: true, username: true, email: true, adminId: true }
-   })
-   if (!currentUser) { throw new Error('User not found.') }
+      const currentUser = await prismadb.user.findUnique({ 
+         where: { email: session.user.email },
+         select: { id: true, username: true, email: true, adminId: true }
+      })
+      if (!currentUser) { throw new Error('User not found.') }
 
-   return { currentUser, adminId: currentUser.adminId };
+      return { currentUser, adminId: currentUser.adminId };
+   } catch (error) {
+      return { currentUser: null, adminId: null, error };
+   }
 }
 
 export default serverAuth;
