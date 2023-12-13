4b75464614a5c23fdeccc8e2f9124e49717e4137
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-13 17:16:47 +0100

    global layout seo

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -1,6 +1,5 @@
+import Head from 'next/head'
 import type { Metadata } from 'next'
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 
 type RootLayoutProps = {
    children: React.ReactNode;
@@ -14,30 +13,27 @@ export default function RootLayout({ children, metadata }: RootLayoutProps){
    const applicationName = metadata?.applicationName?.toString();
    const themeColor = metadata?.themeColor?.toString();
 
-   const router = useRouter();
-   useEffect(() => {
-     if (router.pathname === '/_layout') { router.replace('/') }
-   }, [router]);
-
   return (
    <>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
-      <meta name="author" content={applicationName} />
-      <meta name="theme-color" content={themeColor}/>
-      <meta name="viewport" content="width=device-width, initial-scale=0.8" />
-      <meta httpEquiv="Content-Language" content="ru" />
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="robots" content="index, follow" />
-      <meta name="publisher" content="Vladislav Surnin" />
+      <Head>
+         <title>{title}</title>
+         <meta name="description" content={description} />
+         <meta name="keywords" content={keywords} />
+         <meta name="author" content={applicationName} />
+         <meta name="theme-color" content={themeColor}/>
+         <meta name="viewport" content="width=device-width, initial-scale=0.8" />
+         <meta httpEquiv="Content-Language" content="ru" />
+         <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+         <meta name="robots" content="index, follow" />
+         <meta name="publisher" content="Vladislav Surnin" />
 
-      {/* Open Graph Tags */}
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="/images/logo.png" />
-      <meta property="og:url" content="https://vladnovels.com/" />
+         {/* Open Graph Tags */}
+         <meta property="og:title" content={title} />
+         <meta property="og:description" content={description} />
+         <meta property="og:type" content="website" />
+         <meta property="og:image" content="/images/logo.png" />
+         <meta property="og:url" content="https://vladnovels.com/" />
+      </Head>
 
       <div className="relative h-screen">
          {children}
