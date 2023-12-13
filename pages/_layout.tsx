99955f2ff1b8072c6f6468ab2b29c04becb3c159
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-13 19:22:27 +0100

    metadata

--- pages/_layout.tsx
+++ pages/_layout.tsx
@@ -17,10 +17,11 @@ export default function RootLayout({ children, metadata }: RootLayoutProps){
    <>
       <Head>
          <title>{title}</title>
+         <meta name="title" content={title} />
          <meta name="description" content={description} />
          <meta name="keywords" content={keywords} />
          <meta name="author" content={applicationName} />
-         <meta name="theme-color" content={themeColor}/>
+         <meta name="theme-color" content={themeColor} />
          <meta name="viewport" content="width=device-width, initial-scale=0.8" />
          <meta httpEquiv="Content-Language" content="ru" />
          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
