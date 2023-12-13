763cb69d98f0eba194d3db06052f04aa8ade555f
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-13 20:34:39 +0100

    profile layout v1

--- pages/profile/_layout.tsx
+++ pages/profile/_layout.tsx
@@ -1,15 +1,41 @@
-import { Navbar } from '@/components';
 import React from 'react'
+import Head from 'next/head'
+import { SessionUserProps } from '@/types'
+import { Navbar } from '@/components'
 
 type ProfileLayoutProps = {
    children: React.ReactNode;
+   layoutMetadata: {
+      title: string;
+      description: string;
+      keywords: string;
+      applicationName: string;
+      themeColor: string;
+   };
+   session: SessionUserProps;
 };
 
-export default function ProfileLayout ({ children }: ProfileLayoutProps) {
+export const metadataProfile = {
+   title: 'testing the title seo',
+   description: 'testing the desc seo',
+   keywords: 'testing the keywords seo',
+   applicationName: 'testing the author seo',
+   themeColor: "#2d2e30",
+}
+
+
+export default function ProfileLayout ({ children, layoutMetadata, session }: ProfileLayoutProps) {
   return (
-    <div>
-      <Navbar />
+    <>
+      <Head>
+         <meta name="title" content={layoutMetadata.title} />
+         <meta name="description" content={layoutMetadata.description} />
+         <meta name="keywords" content={layoutMetadata.keywords} />
+         <meta name="author" content={layoutMetadata.applicationName} />
+         <meta name="theme-color" content={layoutMetadata.themeColor} />
+      </Head>
+      <Navbar isUser={!!session?.email} isAdmin={!!session?.adminId} />
       {children}
-   </div>
+   </>
   )
 }
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -9,10 +9,18 @@ import { SessionUserProps } from '@/types'
 import useNovelList from '@/hooks/useNovelList'
 import useNovel from '@/hooks/useNovel'
 
+import type { ReactElement } from 'react'
+import type { NextPageWithLayout } from '@/pages/_app'
+import { metadata } from '@/pages/_app'
+import ProfileLayout from './_layout'
+import { metadataProfile } from './_layout'
+import RootLayout from '../_layout'
+import { ProfileProps } from '@/types'
+ 
 // Protecting routes by fetching user session on client side
 export const getServerSideProps = getUserSessionServerSideProps;
 
-const Profile: React.FC<{ session: SessionUserProps }> = ({ session }) => {
+const Profile: NextPageWithLayout<ProfileProps> = ({ session }) => {
    const router = useRouter();
    const novelId = router.query.novel as string;
 
@@ -34,7 +42,7 @@ const Profile: React.FC<{ session: SessionUserProps }> = ({ session }) => {
       <div className="w-screen min-h-full bg-white-main">
          {/* Top Navigation */}
          {(isLoading || !novels) ? <Loader /> : (<>
-         <div className="fixed w-full px-4 md:px-24 md:pr-14 pr-12 z-30 flex flex-col justify-between gap-4 md:gap-8 bg-black bg-opacity-80">
+         {/* <div className="fixed w-full px-4 md:px-24 md:pr-14 pr-12 z-30 flex flex-col justify-between gap-4 md:gap-8 bg-black bg-opacity-80">
             <nav className="flex flex-row justify-between pb-4 md:pb-1 px-2 md:px-5">
                <div className="flex items-center justify-center px-7 pt-5 md:pt-4 gap-1 md:gap-2 cursor-pointer" onClick={() => signOut()}>
                   <BiLogOut size={32} className="text-white sm:hidden" />
@@ -48,7 +56,7 @@ const Profile: React.FC<{ session: SessionUserProps }> = ({ session }) => {
                   <FaDonate size={28} className="text-white hidden lg:inline" />
                </div>
             </nav>
-         </div>
+         </div> */}
          {/* Main Content */}
          <div className="flex-grow overflow-hidden">
             <Carousel novels={novels} adminPage={false} />
@@ -59,5 +67,14 @@ const Profile: React.FC<{ session: SessionUserProps }> = ({ session }) => {
       </div>
    )
 };
+
+Profile.getLayout = function getLayout(page: ReactElement, props: ProfileProps) {
+   const { session } = props;
+   return (
+      <RootLayout metadata={metadata}>
+         <ProfileLayout layoutMetadata={metadataProfile || metadata} session={props.session}>{page}</ProfileLayout>
+      </RootLayout>
+   )
+}
 
 export default Profile
