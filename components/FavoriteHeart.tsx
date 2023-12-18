896420cf3b49d199997f1b4890fd18a0bd0180f2
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-18 18:17:34 +0100

    favorite v2 done

--- components/FavoriteHeart.tsx
+++ components/FavoriteHeart.tsx
@@ -1,51 +1,67 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button  } from '@/components'
 import { TiHeart } from 'react-icons/ti'
 import { toast } from 'react-hot-toast'
 
 const FavoriteHeart = ({novelId}: { novelId: string | undefined}) => {
-   const [isFavorited, setIsFavorited] = useState();
-   const [loading, setLoading] = useState(false);
+   const [isFavorited, setIsFavorited] = useState<boolean>();
+   const [loadingPost, setLoadingPost] = useState(false);
+   const [loadingDelete, setLoadingDelete] = useState(false);
+
+   useEffect(() => {
+      const fetchUserFavorites = async () => {
+        try {
+          const response = await axios.get('/api/favorite/listAll');
+          const userFavorites = response.data.map((fav: { novelId: string }) => fav.novelId);
+          setIsFavorited(userFavorites.includes(novelId));
+        } catch (error) {
+          console.error('Failed to fetch user favorites', error);
+        }
+      };
+  
+      fetchUserFavorites();
+   }, [novelId]);
 
    const postFav = async () => {
       try {
-         setLoading(true);
-         await axios.post('/api/favorite', novelId);
+         setLoadingPost(true);
+         await axios.post('/api/favorite', { novelId });
 
          console.log('Novel favorited successfully.');
          toast.success('Novel favorited.');
+         setIsFavorited(true);
       } catch (error) {
          console.error('Favorite implementation has failed', error);
          toast.error('An error occurred.');
       } finally {
-         setLoading(false);
+         setLoadingPost(false);
       }
    };
 
    const deleteFav = async () => {
       try {
-         setLoading(true);
+         setLoadingDelete(true);
          await axios.delete(`/api/favorite?novelId=${novelId}`);
 
          console.log('Novel un-favorited successfully.');
          toast.success('Novel un-favorited.');
+         setIsFavorited(false);
       } catch (error) {
          console.error('Favorite removal has failed', error);
          toast.error('An error occurred.');
       } finally {
-         setLoading(false);
+         setLoadingDelete(false);
       }
    };
 
   return (
-    <div className="flex justify-center space-x-2 absolute top-0 right-0">
-      {
-      isFavorited
-     ?
-     <Button btnType="button" action={deleteFav} additionalStyles="button-unfav" reactIcon={<TiHeart size={25} />} isDisabled={loading} />
-     :
-     <Button btnType="button" action={postFav} additionalStyles="button-fav" reactIcon={<TiHeart size={25} />} isDisabled={loading} />
+    <div className="absolute top-0 right-0">
+      {isFavorited
+         ?
+         <Button btnType="button" action={deleteFav} additionalStyles="button-unfav" reactIcon={<TiHeart size={25} />} isDisabled={loadingDelete} />
+         :
+         <Button btnType="button" action={postFav} additionalStyles="button-fav" reactIcon={<TiHeart size={25} />} isDisabled={loadingPost} />
       }
     </div>
   )
