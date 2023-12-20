60d86e6ecf6d7ac414bc8a1d29e4721bdb5ff861
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-12-20 19:20:54 +0100

    fav pagination v4.1

--- pages/profile/lounge/[novelId].tsx
+++ pages/profile/lounge/[novelId].tsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import useNovel from '@/hooks/useNovel'
 import { useRouter } from 'next/router'
 
 const LoungeId = () => {
    const router = useRouter();
-   const { novelId } = router.query;
+   const { novelId, page } = router.query;
    const { data, isLoading, error } = useNovel(novelId as string);
+
+   const currentPage = parseInt(page as string, 10) || 1;
+   const itemsPerPage = 2000;
+   const startIndex = (currentPage - 1) * itemsPerPage;
+   const endIndex = startIndex + itemsPerPage;
+   
+   const currentPageContent = data?.content?.slice(startIndex, endIndex);
+
+   useEffect(() => {
+      // Redirect to a valid page if the current page is out of bounds
+      if (!isNaN(currentPage) && (currentPage < 1 || isNaN(startIndex) || startIndex >= data?.content.length)) {
+        router.push(`/profile/lounge/${novelId}`);
+      }
+   }, [currentPage, startIndex, novelId, data, router]);
 
    if (!novelId || error) { return null }
 
   return (
-    <div className='pt-32 text-center'>LoungeId Page: {novelId} / { data?.title}</div>
+    <>
+      <div className='pt-32 text-center'>LoungeId Page: {novelId} / { data?.title}</div>
+      <div>
+         {currentPageContent}
+      </div>
+    </>
   )
 }
 
