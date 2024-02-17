84344b14c07e80fb22e0047b1cd4483b0fe47f4c
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-02-17 11:19:16 +0100

    update viewport

--- components/NovelList.tsx
+++ components/NovelList.tsx
@@ -13,7 +13,7 @@ const NovelList: React.FC<NovelListProps> = ({ header, data }) => {
    
    return (
       <div className="min-h-full px-10 md:px-16 lg:px-12 space-y-8 pt-3">
-         <div className="lg:pt-0 md:pt-20 sm:pt-16 pt-20 md:pb-4 pb-0">
+         <div className="lg:pt-20 md:pt-20 sm:pt-16 pt-20 md:pb-4 pb-0">
             <p className="title-header text-primary-black md:text-start text-center text-sm md:text-base lg:text-xl font-semibold md:pl-1 lg:mt-2 md:mt-6 mt-4 lg:mb-1 md:mb-2 mb-4">
                {header}
             </p>
